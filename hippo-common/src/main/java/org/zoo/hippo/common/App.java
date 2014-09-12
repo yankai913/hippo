0a07f341908d3cdef3af8aa4a58f9c39e92710cd
yankai913 <yankai913@gmail.com>
2014-09-12 16:09:05 +0800

    init

---
+++ hippo-common/src/main/java/org/zoo/hippo/common/App.java
@@ -0,0 +1,13 @@
+package org.zoo.hippo.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
